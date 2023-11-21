6441b2c39876b55a65e093727634474449d02423
Lakshitha <you@example.com>
2023-11-21 11:35:31 +0530

    Add the product model.

---
+++ Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCRUD.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> opts) 
+        :base (opts){ }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
