b2d3d47017cf0bd299ecb609246b5a29063ceb60
Lakshitha <you@example.com>
2023-11-21 12:41:21 +0530

    Build the ProductController for a test product.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new Product[]
+            {
+                new Product() { Name = "Product 1" },
+                new Product() { Name = "Product 2" },
+
+            };
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "Test Product"
+            };
+        }
+    }
+}
