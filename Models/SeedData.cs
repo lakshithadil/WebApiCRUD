0f6c33d3032873582d7e79f4aad75d4b3ce10eb3
Lakshitha <you@example.com>
2023-11-21 11:54:52 +0530

    Add SeedData file.

---
+++ Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCRUD.Models
+{
+    public static class SeedData
+    {
+        public static void SeedDatabase(DataContext context)
+        {
+            context.Database.Migrate();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product {
+                    Name = "Kayak", Description = "A boat for one person",
+                    Category = "Watersports", Price = 275
+                },
+                new Product
+                {
+                    Name = "Lifejacket",
+                    Description = "Protective and fashionable",
+                    Category = "Watersports",
+                    Price = 48.95m
+                },
+                new Product
+                {
+                    Name = "Soccer Ball",
+                    Description = "FIFA-approved size and weight",
+                    Category = "Soccer",
+                    Price = 19.50m
+                },
+                new Product
+                {
+                    Name = "Corner Flags",
+                    Description = "Give your playing field a professional touch",
+                    Category = "Soccer",
+                    Price = 34.95m
+                }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
